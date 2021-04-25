617295d1508ce9d1a98f578c272742f02def11cd
Murilo Machado <murilovmachado@gmail.com>
2021-04-25 23:45:23 +0100

    Makes npm command visible to CodeDeplopy

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Make npm command visible
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 # This script is executed after the source is copied to the instances
 cd /home/ec2-user/idle-game-server
 npm install
